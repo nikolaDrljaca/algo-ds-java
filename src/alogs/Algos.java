97364e96d8e91230c0febac493f8fd46e375c05d
nikolaDrljaca <devnikoladr@gmail.com>
2023-05-26 12:40:42 +0300

    quicksort impl

--- src/alogs/Algos.java
+++ src/alogs/Algos.java
@@ -19,7 +19,35 @@ public class Algos {
         return false;
     }
 
-    public static void quickSort(int[] arr) {
+    private static int partition(int[] arr, int lo, int hi) {
+        int pivot = arr[hi];
+        int idx = lo - 1;
 
+        for (int i = lo; i < hi; i++) {
+            if (arr[i] <= pivot) {
+                idx++;
+                int temp = arr[i];
+                arr[i] = arr[idx];
+                arr[idx] = temp;
+            }
+        }
+        idx++;
+        arr[hi] = arr[idx];
+        arr[idx] = pivot;
+
+        return idx;
+    }
+
+    private static void qs(int[] arr, int lo, int hi) {
+        if (lo >= hi) {
+            return;
+        }
+        int pivot = partition(arr, lo, hi);
+        qs(arr, lo, pivot - 1);
+        qs(arr, pivot + 1, hi);
+    }
+
+    public static void quickSort(int[] arr) {
+        qs(arr, 0, arr.length - 1);
     }
 }
